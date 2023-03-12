7846f312a419a39bb5ba489029e4231289e06f95
YoungHo Kim <kim0518@gmail.com>
2023-03-12 13:59:23 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Dinner')
+streamlit.title("My Mom's New Healthy Dinner")
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blackberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 and Blackberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
